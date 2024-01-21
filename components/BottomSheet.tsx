c792ce60d30c1fd20a022f12aedc6cf1f63a86b4
Dmytro Soltysiuk <dimasoltusyuk@gmail.com>
2024-01-20 17:56:35 -0800

    feat: db works

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -1,23 +1,22 @@
 import { default as GorhomBottomSheet } from "@gorhom/bottom-sheet";
-import { useRef, useMemo } from "react";
+import { useRef, useMemo, useState } from "react";
 import { Button, Input, Text, ToggleGroup } from "tamagui";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { supabase } from "../utils/supabase";
 
-const insertRiderRoute = async (destinationPlaceId: any) => {
-  const { error } = await supabase
+const insertRoute = async (destinationPlaceId: any) => {
+  const res = await supabase
     .from("routes")
     .insert({ created_by: "rider", destination_place_id: destinationPlaceId });
-
-  if (error) {
-    console.log("ERROR", error);
-    return;
-  }
+  console.log(res);
 };
 
 const BottomSheet = () => {
   const bottomSheetRef = useRef<GorhomBottomSheet>(null);
   const snapPoints = useMemo(() => ["25%", "50%"], []);
+  const [destinationPlaceId, setDestinationPlaceId] = useState<string | null>(
+    null
+  );
 
   return (
     <GorhomBottomSheet ref={bottomSheetRef} index={1} snapPoints={snapPoints}>
@@ -33,9 +32,8 @@ const BottomSheet = () => {
       <Input size="$4" borderWidth={2} placeholder="Phone" />
       <GooglePlacesAutocomplete
         placeholder="Enter your destination"
-        onPress={(data, details = null) => {
-          console.log(data, details);
-          insertRiderRoute(data.place_id);
+        onPress={(data) => {
+          setDestinationPlaceId(data.place_id);
         }}
         query={{
           key: "AIzaSyDn9nTNd7etbjiCTE3IwYnGoCi9kZwBR7w",
@@ -43,11 +41,9 @@ const BottomSheet = () => {
         }}
         textInputProps={{
           InputComp: Input,
-          // leftIcon: { type: "font-awesome", name: "chevron-left" },
-          // errorStyle: { color: "red" },
         }}
       />
-      <Button>Go!</Button>
+      <Button onPress={() => insertRoute(destinationPlaceId)}>Go!</Button>
     </GorhomBottomSheet>
   );
 };
